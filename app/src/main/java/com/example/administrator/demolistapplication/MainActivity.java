589a144955e05a45048110d1eb059525d5588b97
zrh <1354709860@qq.com>
2018-07-20 09:04:30 +0800

    添加了动态生成key列表工具

--- app/src/main/java/com/example/administrator/demolistapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/demolistapplication/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.administrator.demolistapplication;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.example.administrator.demolistapplication.demomoduls.createFileDemo.CreateJavaFileActivity;
 import com.example.administrator.demolistapplication.demomoduls.quanpin_adjustresize.QP_ResizeTestActivity;
 import com.example.administrator.demolistapplication.demomoduls.rxjava_study.RxjavaBaseActivity;
 
@@ -23,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         this.mainlistview = (ListView) findViewById(R.id.main_listview);
 
+        onGetQuanxian();
         onInit();
 
     }
@@ -33,11 +39,13 @@ public class MainActivity extends AppCompatActivity {
         final ArrayList<String> listTitle = new ArrayList<>();
         listTitle.add( "01 全屏与adjustResize冲突问题处理" );
         listTitle.add( "02 rxjava2 使用" );
+        listTitle.add( "03 创建 文件" );
 
 
         final ArrayList<Class> listClass = new ArrayList<>();
         listClass.add(  QP_ResizeTestActivity.class);
         listClass.add(  RxjavaBaseActivity.class);
+        listClass.add(  CreateJavaFileActivity.class);
 
         ArrayAdapter simpleAdapter = new ArrayAdapter(this , android.R.layout.simple_list_item_1 , listTitle );
         mainlistview.setAdapter(simpleAdapter);
@@ -52,5 +60,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    /**
+     * 6.0  系统 检查运行权限
+     */
+    private void onGetQuanxian() {
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE}, 99);
+        } else {
+            onCallRun();
+        }
+    }
 
+    private void onCallRun() {
+        //初始化数据库
+
+    }
 }
